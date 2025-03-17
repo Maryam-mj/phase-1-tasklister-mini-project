4d945da975ca10b624d09769551bff9a1bdab244
Maryam-mj <mariamaliabdi12@gmail.com>
2025-03-17 05:05:17 -0700

    add assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+  const formInput = document.querySelector("#new-task-description");
+  const taskList = document.querySelector("#tasks");
+
+  form.addEventListener("submit",(event) => {
+    
+    if (formInput.value.trim() !== "") {
+      const newTask = document.createElement("li");
+      newTask.textContent = formInput.value;
+      taskList.append(newTask);
+
+      formInput.value = "";
+    }
+  });
 });
+
